15bc19ab1837c18c58cd15c3dad90067580228be
studente <studente@liceospalla.local>
2018-11-28 11:26:29 +0100

    work

--- esempio.py
+++ esempio.py
@@ -1,13 +1,18 @@
 import requests
-
+#accreditamento
 requests.post("http://192.168.1.231:8080/accreditamento", json = {"nome": "Francesco Musi"})
 
+#ottieni il dict con consegna e dati
 r = requests.get("http://192.168.1.231:8080/esercizi/1").json()
+#stampa consegna e dati
 print(r)
 
+#svolgimento
 out = []
 for i in r["data"]:
-	#svolgimento
+	
 
+#manda la risposta
 r = request.post("http://192.168.1.231:8080/esercizi/1", json = {"data": out})
+#dice se giusto/sbagliato
 print(r.json())
