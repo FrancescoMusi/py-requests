858fc03c541306c25c569d1648c581fb17301bce
Francesco Musi <37630916+FrancescoMusi@users.noreply.github.com>
2019-05-06 09:45:36 +0200

    Update esempio.py

--- esempio.py
+++ esempio.py
@@ -1,18 +1,38 @@
 import requests
+import os
+#20
+
+esercizio = '29'
+
+'''
 #accreditamento
-requests.post("http://192.168.1.231:8080/accreditamento", json = {"nome": "Francesco Musi"})
+accr = requests.post("http://192.168.1.231:8080/accreditamento", json = {"nome": "Francesco Musi"})
+print(accr.json())
+'''
 
 #ottieni il dict con consegna e dati
-r = requests.get("http://192.168.1.231:8080/esercizi/1", headers = {"x-data", "true"}).json()
-#stampa consegna e dati
-print(r)
+r = requests.get("http://192.168.1.231:8080/esercizi/{}".format(esercizio), headers = {"x-data": "true"})
 
-#svolgimento
-out = []
-for i in r["data"]:
-	
+message = r.json()['message']
+data = r.json()['data']
+
+os.system('cls')
+print()
+print(message)
+print()
+print(data)
+print()
+
+
+
+out = 1
+for i in range(1, data+1):
+	out *= i
+
+
+print(out)
+print()
 
 #manda la risposta
-r = requests.post("http://192.168.1.231:8080/esercizi/1", json = {"data": out})
-#dice se giusto/sbagliato
-print(r.json())
+res = requests.post("http://192.168.1.231:8080/esercizi/{}".format(esercizio), json = {"data": out})
+print(res.json()['message'])
