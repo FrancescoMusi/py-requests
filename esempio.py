d8697904f1f27f0bc7c7e4ebe8f9abf397963d14
Francesco Musi <37630916+FrancescoMusi@users.noreply.github.com>
2019-05-08 11:47:18 +0200

    Update esempio.py

--- esempio.py
+++ esempio.py
@@ -1,15 +1,14 @@
 import requests
 import os
-#20
 
-esercizio = '29'
+esercizio = '30'
 
 '''
 #accreditamento
 accr = requests.post("http://192.168.1.231:8080/accreditamento", json = {"nome": "Francesco Musi"})
 print(accr.json())
-'''
 
+'''
 #ottieni il dict con consegna e dati
 r = requests.get("http://192.168.1.231:8080/esercizi/{}".format(esercizio), headers = {"x-data": "true"})
 
@@ -23,15 +22,25 @@ print()
 print(data)
 print()
 
+x = 0
+y = 0
+field = []
 
+field = data.split('\n')
+print(field)
+out = []
 
-out = 1
-for i in range(1, data+1):
-	out *= i
+for i in field:
+	if 'X' in i:
+		x = i.index('X')
+		y = field.index(i)
 
+print(x, y)
 
-print(out)
-print()
+out = {
+	'x': x,
+	'y': y
+}
 
 #manda la risposta
 res = requests.post("http://192.168.1.231:8080/esercizi/{}".format(esercizio), json = {"data": out})
